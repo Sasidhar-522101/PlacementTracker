80914e2292b0fc74d23f9255a58e373249369867
Sasidhar <saisasidhar701@gmail.com>
2025-08-14 11:34:29 +0530

    Initial commit - Base Spring Boot project

---
+++ src/main/java/com/placement/project/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.placement.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.placement.project.model.Student;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer>{
+	
+	public Student findByIdAndPassword(Long id, String password);
+}
